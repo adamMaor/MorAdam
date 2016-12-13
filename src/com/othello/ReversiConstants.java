d49ff6a1c9ac388dc39ae63514bc15aa754ed789
adamMaor <maorcpa.adam@gmail.com>
2016-12-13 16:42:21 +0200

    Good Commit

--- src/com/othello/ReversiConstants.java
+++ src/com/othello/ReversiConstants.java
@@ -11,9 +11,9 @@ public class ReversiConstants {
     public static int boardWidth = 12;
     public static int boardSquare = boardHeight*boardWidth;
     public static Color reversiGreen = new Color(0, 150, 0);
-    public static long PCDelayTime = 500;
-
-    public enum Directions {N , NE, E, SE, S ,SW ,W, NW};
-
+    public static Color reversiWhite = Color.white;
+    public static Color reversiBlack = new Color(30,30,30);
+    public static Color lightMiddle = new Color(180,180,180);
+    public static Color darkMiddle = new Color (60,60,60);
 }
 
